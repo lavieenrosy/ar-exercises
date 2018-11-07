e08c448f36688514c9c33c418dd43811aea744de
Rosy Lee <rosyhlee@gmail.com>
2018-11-07 00:58:28 +0000

    finished exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -12,9 +12,10 @@ puts "----------"
 # Your code goes here ...
 
 class Employee
+  belongs_to :store
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates_associated :stores
+  validates_associated :store
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
 end
 
@@ -24,11 +25,27 @@ class Store
   validate :must_have_apparel
 
   def must_have_apparel
-    if mens_apparel = false || womens_apparel = false
-      errors.add("must carry mens or womens apparel")
+    if (mens_apparel = false || !mens_apparel.present?) || (womens_apparel = false || !womens_apparel.present?)
+      errors.add(:mens_apparel, "must carry mens apparel")
+      errors.add(:womens_apparel, "must carry womens apparel")
     end
   end
 
 end
 
+puts "Please input a store name:"
+print "> "
+store_name = $stdin.gets.chomp
+new_store = Store.create(name: store_name)
+p new_store.mens_apparel.present?
+new_store.save
 
+p new_store.errors.messages
+
+puts "Please input an employee name:"
+print "> "
+employee_name = $stdin.gets.chomp
+new_employee = Employee.create(first_name: employee_name)
+new_employee.save
+
+p new_employee.errors.messages
